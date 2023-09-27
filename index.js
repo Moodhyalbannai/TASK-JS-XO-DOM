470e9ef9c9fcc4e7a5a781b0345cbab78ca10984
Moodhyalbannai <mood_92@hotmail.com>
2023-09-27 17:30:04 +0300

    push

--- index.js
+++ index.js
@@ -3,19 +3,30 @@
 // First button top left is called 1, last button bottom right is 9
 function fillButton(index, text) {
   // This function fills the button of the send index
+  
   document.getElementById(index).innerHTML = text;
 }
 // pre-made a function. You can use this function to present an alert to say someone wins
+let winningConditions = [
+  [1,2,3], [4,5,6], [7,8,9],
+  [1,4,7], [2,5,8], [3,6,9],
+  [1,5,9], [3,5,7]
+  ];
+
 function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
+  if(confirm(`Horraaay, ${winner} wins!`))
      // The code here will be exectued if you press on OK button that will pop on the window 
+
+ 
   }
-}
+
 
 // SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
 // ❗️ Delete this code once you are done testing
 fillButton(1, "X");
 fillButton(9, "O");
+
+
 
 /**
  *
@@ -34,3 +45,5 @@ function clickButton(index) {
  */
 // function checkWinner
 // function restartGame
+
+
